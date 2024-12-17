dcff8aaf935c16340d8bf3f78a267846628e25e3
MargotPasquali <margotpasquali1@gmail.com>
2024-12-17 17:17:38 +0100

    add custom .gitignore

---
+++ Sources/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Margot Pasquali on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
