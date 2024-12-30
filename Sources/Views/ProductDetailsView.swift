4fc3f739f6e697e7fc0b58eef6993b8ebd428bcc
MargotPasquali <margotpasquali1@gmail.com>
2024-12-30 16:37:51 +0100

    modify DetailView

--- Sources/Views/ProductDetailsView.swift
+++ Sources/Views/ProductDetailsView.swift
@@ -52,67 +52,60 @@ struct ProductDetailsView: View {
                         EmptyView()
                     }
                 }
-                .padding(.horizontal, 15.0)
+                Image("Share")
+                    .offset(x: -12, y: -190)
                 
-                HStack {
-                    Text("Donnée 'description' manquante. Il s'agit d'une description par défaut")
-                        .padding(.leading)
-                    Spacer()
-                }
-                
-                // MARK: - Rating Section
-                HStack {
-                    Image("Persona")
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(Circle())
-                        .frame(width: 40, height: 40)
-                        .padding(.trailing, 10.0)
-                        .padding(.leading, 15.0)
-                        .foregroundStyle(Color.gray)
-                    
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Spacer()
-                }
-                
-                RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.gray)
-                    .overlay(
-                        Text("Partagez ici vos impressions sur cette pièce")
-                            .fontWeight(.regular)
-                            .foregroundColor(.gray)
-                            .font(.headline)
-                            .multilineTextAlignment(.leading)
-                    )
-                    .frame(height: 68)
-                    .padding(15.0)
-                
-                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.white)
+                        .frame(width: 51, height: 27)
+                    HStack {
+                        Image(systemName: "heart")
+                            .foregroundStyle(Color.black)
+                            .frame(width: 14, height: 12)
+                        Text(String(clothes.likes))
+                            .fontWeight(.semibold)
+                            .foregroundStyle(Color.black)
+                            .font(.caption)
+                    }
+                }.offset(x: -10, y: 190)
             }
+            // MARK: - Product Name and Rating
+            
+            HStack {
+                Text(clothes.name)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .foregroundStyle(Color.black)
+                Spacer()
+                Image(systemName: "star.fill")
+                    .foregroundStyle(Color.yellow)
+                    .frame(width: 12, height: 12)
+                Text(String(format: "%.1f", clothes.price))
+                    .font(.title3)
+                    .fontWeight(.regular)
+                    .foregroundStyle(Color.black)
+            }
+            .padding(.horizontal, 15.0)
+            
+            // MARK: - Price Section
+            HStack {
+                Text(String(format: "%.2f €", clothes.price))
+                    .font(.title3)
+                    .fontWeight(.regular)
+                    .foregroundStyle(Color.black)
+                Spacer()
+                if clothes.originalPrice != clothes.price {
+                    Text(String(format: "%.2f €", clothes.originalPrice))
+                        .font(.title3)
+                        .fontWeight(.regular)
+                        .foregroundStyle(Color.gray)
+                        .strikethrough()
+                }
+            }
+            .padding(.horizontal, 15.0)
+            Text(clothes.picture.description)
         }
     }
 }
