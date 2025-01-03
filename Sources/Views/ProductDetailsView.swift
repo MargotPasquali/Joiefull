902110d134404a85d929dec8d1c0860caa362c9a
MargotPasquali <margotpasquali1@gmail.com>
2025-01-03 15:52:34 +0100

    WIP - ProductDetailsView Implementation

--- Sources/Views/ProductDetailsView.swift
+++ Sources/Views/ProductDetailsView.swift
@@ -54,121 +54,149 @@ struct ProductDetailsView: View {
                     Image("Share")
                         .offset(x: -12, y: -190)
                     
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color.white)
-                            .frame(width: 51, height: 27)
-                        HStack {
-                            Image(systemName: "heart")
-                                .foregroundStyle(Color.black)
-                                .frame(width: 14, height: 12)
-                            Text(String(product.likes)) // Utilisation correcte de product.likes
-                                .fontWeight(.semibold)
-                                .foregroundStyle(Color.black)
-                                .font(.caption)
+                    VStack {
+                        ZStack(alignment: .trailing) {
+                            // Chargement de l'image
+                            if let url = product.picture.imageURL { // Utilisation correcte de imageURL
+                                AsyncImage(url: url) { phase in
+                                    switch phase {
+                                    case .empty:
+                                        ProgressView()
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 369, height: 431)
+                                            .cornerRadius(20)
+                                    case .failure:
+                                        Image(systemName: "photo")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 369, height: 431)
+                                            .foregroundColor(.gray)
+                                    @unknown default:
+                                        EmptyView()
+                                    }
+                                }
+                            }
+                            Image("Share")
+                                .offset(x: -12, y: -190)
+                            
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.white)
+                                    .frame(width: 51, height: 27)
+                                HStack {
+                                    Image(systemName: "heart")
+                                        .foregroundStyle(Color.black)
+                                        .frame(width: 14, height: 12)
+                                    Text(String(product.likes)) // Utilisation correcte de product.likes
+                                        .fontWeight(.semibold)
+                                        .foregroundStyle(Color.black)
+                                        .font(.caption)
+                                }
+                            }
+                            .offset(x: -10, y: 190)
                         }
+                        // MARK: - Product Name and Rating
+                        
+                        HStack {
+                            Text(product.name)
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .foregroundStyle(Color.black)
+                            Spacer()
+                            Image(systemName: "star.fill")
+                                .foregroundStyle(Color.yellow)
+                                .frame(width: 12, height: 12)
+                            Text("4.5")
+                                .font(.title3)
+                                .fontWeight(.regular)
+                                .foregroundStyle(Color.black)
+                        }
+                        .padding(.horizontal, 15.0)
+                        
+                        // MARK: - Price Section
+                        HStack {
+                            Text(String(format: "%.2f €", product.price))
+                                .font(.title3)
+                                .fontWeight(.regular)
+                                .foregroundStyle(Color.black)
+                            Spacer()
+                            if product.originalPrice != product.price {
+                                Text(String(format: "%.2f €", product.originalPrice))
+                                    .font(.title3)
+                                    .fontWeight(.regular)
+                                    .foregroundStyle(Color.gray)
+                                    .strikethrough()
+                            }
+                        }
+                        .padding(.horizontal, 15.0)
+                        
+                        HStack {
+                            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                                .padding(.leading)
+                            Spacer()
+                        }
+                        
+                        // MARK: - Rating Section
+                        HStack {
+                            Image("Persona")
+                                .resizable()
+                                .scaledToFill()
+                                .clipShape(Circle())
+                                .frame(width: 40, height: 40)
+                                .padding(.trailing, 10.0)
+                                .padding(.leading, 15.0)
+                                .foregroundStyle(Color.gray)
+                            
+                            Image(systemName: "star")
+                                .resizable()
+                                .foregroundStyle(Color.gray)
+                                .frame(width: 25, height: 25)
+                                .padding(.trailing, 3.0)
+                            Image(systemName: "star")
+                                .resizable()
+                                .foregroundStyle(Color.gray)
+                                .frame(width: 25, height: 25)
+                                .padding(.trailing, 3.0)
+                            Image(systemName: "star")
+                                .resizable()
+                                .foregroundStyle(Color.gray)
+                                .frame(width: 25, height: 25)
+                                .padding(.trailing, 3.0)
+                            Image(systemName: "star")
+                                .resizable()
+                                .foregroundStyle(Color.gray)
+                                .frame(width: 25, height: 25)
+                                .padding(.trailing, 3.0)
+                            Image(systemName: "star")
+                                .resizable()
+                                .foregroundStyle(Color.gray)
+                                .frame(width: 25, height: 25)
+                                .padding(.trailing, 3.0)
+                            Spacer()
+                        }
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray)
+                            .overlay(
+                                Text("Partagez ici vos impressions sur cette pièce")
+                                    .fontWeight(.regular)
+                                    .foregroundColor(.gray)
+                                    .font(.headline)
+                                    .multilineTextAlignment(.leading)
+                            )
+                            .frame(height: 68)
+                            .padding(15.0)
+                        
+                        
                     }
-                    .offset(x: -10, y: 190)
                 }
-                // MARK: - Product Name and Rating
-                
-                HStack {
-                    Text(product.name)
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .lineLimit(1)
-                        .foregroundStyle(Color.black)
-                    Spacer()
-                    Image(systemName: "star.fill")
-                        .foregroundStyle(Color.yellow)
-                        .frame(width: 12, height: 12)
-                    Text("4.5")
-                        .font(.title3)
-                        .fontWeight(.regular)
-                        .foregroundStyle(Color.black)
-                }
-                .padding(.horizontal, 15.0)
-                
-                // MARK: - Price Section
-                HStack {
-                    Text(String(format: "%.2f €", product.price))
-                        .font(.title3)
-                        .fontWeight(.regular)
-                        .foregroundStyle(Color.black)
-                    Spacer()
-                    if product.originalPrice != product.price {
-                        Text(String(format: "%.2f €", product.originalPrice))
-                            .font(.title3)
-                            .fontWeight(.regular)
-                            .foregroundStyle(Color.gray)
-                            .strikethrough()
-                    }
-                }
-                .padding(.horizontal, 15.0)
-                
-                HStack {
-                    Text("Donnée 'description' manquante. Il s'agit d'une description par défaut")
-                        .padding(.leading)
-                    Spacer()
-                }
-                
-                // MARK: - Rating Section
-                HStack {
-                    Image("Persona")
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(Circle())
-                        .frame(width: 40, height: 40)
-                        .padding(.trailing, 10.0)
-                        .padding(.leading, 15.0)
-                        .foregroundStyle(Color.gray)
-                    
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Image(systemName: "star")
-                        .resizable()
-                        .foregroundStyle(Color.gray)
-                        .frame(width: 25, height: 25)
-                        .padding(.trailing, 3.0)
-                    Spacer()
-                }
-                
-                RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.gray)
-                    .overlay(
-                        Text("Partagez ici vos impressions sur cette pièce")
-                            .fontWeight(.regular)
-                            .foregroundColor(.gray)
-                            .font(.headline)
-                            .multilineTextAlignment(.leading)
-                    )
-                    .frame(height: 68)
-                    .padding(15.0)
-                
-                
             }
         }
     }
-}
-
 
 // MARK: - Preview
 #Preview {
